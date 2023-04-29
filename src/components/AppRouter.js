17c596af70f6f56adb7d6908bfa6307582cb5b67
brndalx <brndalx@gmail.com>
2023-04-30 01:08:38 +0300

    feat: redirect if page is wrong

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/const";
 
 export default function AppRouter() {
   const isAuth = false;
@@ -13,6 +14,7 @@ export default function AppRouter() {
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} component={Component} exact />
       ))}
+      <Redirect to={SHOP_ROUTE} />
     </Switch>
   );
 }
