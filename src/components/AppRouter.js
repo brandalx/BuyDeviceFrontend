0f6dbe8e27942aa007c7ab9d695e0a88dea774c0
brndalx <brndalx@gmail.com>
2023-04-30 01:06:37 +0300

    feat: appRouter map throught the routes

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,5 +1,18 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+
 export default function AppRouter() {
-  return <div>AppRouter</div>;
+  const isAuth = false;
+  return (
+    <Switch>
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} component={Component} exact />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+    </Switch>
+  );
 }
